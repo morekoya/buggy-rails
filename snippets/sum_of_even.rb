ec0fa7e7f83c822a736fd1b918337f77ccdc5d06
Morolake Orekoya <morolake.orekoya@andela.com>
2018-09-10 12:35:07 +0100

    debug sum_of_even

--- snippets/sum_of_even.rb
+++ snippets/sum_of_even.rb
@@ -1,4 +1,5 @@
 def sum_of_even(min, max)
-  even_numbers = (min...max).select { |n| n%2 === 0 }
-  sum = even_numbers.Sum
+  even_numbers = (min...(max + 1)).select { |n| (n % 2) == 0 }
+  sum = 0
+  even_numbers.map { |n| sum = sum + n }.last
 end
