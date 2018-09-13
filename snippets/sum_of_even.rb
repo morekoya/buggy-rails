b7646ce50e63c4677b872727723dfb4501ea020b
Morolake Orekoya <morolake.orekoya@andela.com>
2018-09-13 15:57:25 +0100

    replace map with reduce

--- snippets/sum_of_even.rb
+++ snippets/sum_of_even.rb
@@ -1,5 +1,4 @@
 def sum_of_even(min, max)
   even_numbers = (min...(max + 1)).select { |n| (n % 2) == 0 }
-  sum = 0
-  even_numbers.map { |n| sum = sum + n }.last
+  even_numbers.reduce(0, :+)
 end
